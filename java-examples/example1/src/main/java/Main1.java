5ceae18b99cae9070da8df2551dcac60a325447c
juligove <jagonzalezv@sura.com.co>
2020-06-23 15:41:24 -0500

    self training

---
+++ java-examples/example1/src/main/java/Main1.java
@@ -0,0 +1,13 @@
+
+import io.micronaut.context.env.Environment;
+
+public class Main1 {
+    static void eg1(){
+        System.out.println("eg1 ----> MUST BE IMPLEMENTED");
+    }
+    public static void main(String ... args){
+        eg1();
+
+        Environment environment = new Envi
+    }
+}
